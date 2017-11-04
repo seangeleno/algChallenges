70cf02175b0db43e16ae60c153e09e696d586678
Lyle Hubbard <lhubbard@pdx.edu>
2017-11-04 05:53:46 -0700

    changes to super_sum.py. was ugly, now not as ugly

--- super_sum.py
+++ super_sum.py
@@ -13,16 +13,20 @@ def main( arg_number, arg_iterations ):
         
 
     num = int(concat_to_pass_in)
-    def sum_super_digit( in_int_num, count ):
-        if( len(str(in_int_num)) == 1):
+    print(sum_super_digit(num,0))
+
+
+
+
+def sum_super_digit( in_int_num, count ):
+    if( len(str(in_int_num)) == 1):
             return in_int_num
     
-        concat_sum = str(in_int_num)
-        output = 0
-        for i in range(0,len(str(in_int_num))):    
-            output+=int(concat_sum[i])
-        return sum_super_digit( output, count+1 )
-    print(sum_super_digit(num,0))
+    concat_sum = str(in_int_num)
+    output = 0
+    for i in range(0,len(str(in_int_num))):    
+        output+=int(concat_sum[i])
+    return sum_super_digit( output, count+1 )
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser() 
