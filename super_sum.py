421f78d68e4e5ffcbabbbd24263d5b4dd9f50dcc
Lyle Hubbard III <lhubbard@pdx.edu>
2017-11-17 13:01:58 -0800

    Update super_sum.py

--- super_sum.py
+++ super_sum.py
@@ -1,9 +1,11 @@
 import argparse
 
 def main( arg_number, arg_iterations ):
-    num = int( arg_number )
-    iterations = int( arg_iterations )
-    string_num = str(num)
+    num = int( arg_number ) # const
+    iterations = int( arg_iterations )  #these typecasts allow for a "const" style use. Only once is it done, as opposed to 
+    #the beginning of every loop
+    string_num = str(num)  #same with this type casting operation.  Slight restructuring increases python runtime performace
+    #significantly and substantially
     #print("string num ", string_num, "\niterations ", iterations)
     i = 1
     concat_to_pass_in = string_num
@@ -22,9 +24,10 @@ def sum_super_digit( in_int_num, count ):
     if( len(str(in_int_num)) == 1):
             return in_int_num
     
-    concat_sum = str(in_int_num)
+    concat_sum = str(in_int_num) # "const"
     output = 0
-    for i in range(0,len(str(in_int_num))):    
+    len_str__in_int_num = len(str(in_int_num))  #I also like to include types in the name.  Easier to know properties this way
+    for i in range(0,len_str__in_int_num):    
         output+=int(concat_sum[i])
     return sum_super_digit( output, count+1 )
     
